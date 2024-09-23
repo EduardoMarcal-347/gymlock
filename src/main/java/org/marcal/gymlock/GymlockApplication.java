ebe8d7908e5673c38852202f1c3e8daad9a4fbc3
Eduardo Marçal <edumsfilho347@gmail.com>
2024-09-23 17:37:17 -0300

    initlzng project

---
+++ src/main/java/org/marcal/gymlock/GymlockApplication.java
@@ -0,0 +1,13 @@
+package org.marcal.gymlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymlockApplication.class, args);
+    }
+
+}
